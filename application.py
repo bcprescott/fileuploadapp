0643523adb3075fd637259bf39fcdd91c816f451
Ben Prescott <bcprescott@outlook.com>
2021-11-15 21:10:26 -0600

    Update application.py

--- application.py
+++ application.py
@@ -1,23 +1,32 @@
-from flask import Flask, render_template, request
-from azure.storage.blob import BlobClient
+# from flask import Flask, render_template, request
+# from azure.storage.blob import BlobClient
 
+# app = Flask(__name__)
+
+# @app.route('/')
+# def upload_file():
+#    return render_template('upload.html')
+	
+# # @app.route('/uploader', methods = ['GET', 'POST'])
+# # def upload():
+# #    if request.method == 'POST':
+# #       f = request.files['file']
+# #       f.save(f.filename)
+# #       storage_account_key = "LSRjcStHbWkkpPEVhtWdLfqj/LnhSQ52JEfTIRuXaHPDRESWTFUiurvqW2YZl6McnXoCQp1HVt28YDg3XC7k3A=="
+# #       storage_url = "https://mlstor.blob.core.windows.net/"
+# #       blob_client = BlobClient(storage_url, container_name="images", blob_name=f.filename, credential=storage_account_key)
+# #       with open(f.filename, "rb") as data:
+# #           blob_client.upload_blob(data)
+# #       return "File Uploaded Successfully"
+		
+# if __name__ == '__main__':
+#    app.run(host='127.0.0.1', port=8080, debug=True)
+
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def upload_file():
-   return render_template('upload.html')
-	
-# @app.route('/uploader', methods = ['GET', 'POST'])
-# def upload():
-#    if request.method == 'POST':
-#       f = request.files['file']
-#       f.save(f.filename)
-#       storage_account_key = "LSRjcStHbWkkpPEVhtWdLfqj/LnhSQ52JEfTIRuXaHPDRESWTFUiurvqW2YZl6McnXoCQp1HVt28YDg3XC7k3A=="
-#       storage_url = "https://mlstor.blob.core.windows.net/"
-#       blob_client = BlobClient(storage_url, container_name="images", blob_name=f.filename, credential=storage_account_key)
-#       with open(f.filename, "rb") as data:
-#           blob_client.upload_blob(data)
-#       return "File Uploaded Successfully"
-		
+def home():
+   return render_template('index.html')
 if __name__ == '__main__':
-   app.run(host='127.0.0.1', port=8080, debug=True)
+   app.run()
